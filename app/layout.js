5e5b35f7d7ff0422a596f33de02e27d3b0132ad9
mollydbennett-glitch <mollydbennett@gmail.com>
2026-06-28 10:13:05 -0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,8 +12,10 @@ export default function RootLayout({ children }) {
         <header className="topbar">
           <div className="wrap topbar-inner">
             <a href="/" className="brand">Iris</a>
-            <a href="/upload" className="navlink">Add items</a>
+            <a href="/style" className="navlink">What to wear</a>
             <a href="/wardrobe" className="navlink">Wardrobe</a>
+            <a href="/upload" className="navlink">Add items</a>
+            <a href="/settings" className="navlink">Settings</a>
           </div>
         </header>
         <main className="wrap" style={{ paddingTop: 36, paddingBottom: 80 }}>
